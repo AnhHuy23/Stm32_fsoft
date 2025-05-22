99f0af130149e2e6c204b3a91ad6983a2a9bfc54
Le Anh Huy <hm.quy.9223@gmail.com>
2025-05-22 15:35:04 +0700

    Update DS1307.h

--- DS1307.h
+++ DS1307.h
@@ -1,18 +1,36 @@
-#ifndef __DS1307_H__
-#define __DS1307_H__
+#ifndef DS1307_H
+#define DS1307_H
 
-#include "stm32f4xx_hal.h"
+#include "stm32f4xx.h" 
+#include <stdint.h>
 
-#define DS1307_ADDRESS 0x68 << 1
+#define DS1307_I2C_ADDR 0x68 /* 7-bit I2C address of DS1307 */
 
+/**
+ * @brief Time structure for RTC
+ */
 typedef struct {
-    uint8_t hour;
-    uint8_t minute;
-    uint8_t second;
+    uint8_t seconds; /* Seconds value 0-59 */
+    uint8_t minutes; /* Minutes value 0-59 */
+    uint8_t hours;   /* Hours value 0-23 (24-hour format) */
 } RTC_Time;
 
-void DS1307_Init(I2C_HandleTypeDef *hi2c);
-void DS1307_GetTime(RTC_Time *time);
-void DS1307_SetTime(RTC_Time *time);
+/**
+ * @brief Initialize DS1307 interface
+ * @param i2c Pointer to I2C peripheral (I2C1, I2C2, etc.)
+ */
+void DS1307_Init(I2C_TypeDef *i2c);
 
-#endif
+/**
+ * @brief Read current time from RTC
+ * @param time Pointer to RTC_Time structure to store result
+ */
+void DS1307_ReadTime(RTC_Time *time);
+
+/**
+ * @brief Write new time to RTC
+ * @param time Pointer to RTC_Time structure containing new time
+ */
+void DS1307_WriteTime(RTC_Time *time);
+
+#endif 
